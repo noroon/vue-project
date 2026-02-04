27e4306d233b294ff4990c754c6d979e96f5e262
noroon <noncha.dev@pm.me>
2026-02-04 21:34:31 +0100

    refactor(components): rename single-word components to multi-word names

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -15,9 +15,9 @@ const props = withDefaults(defineProps<TitleProps>(), {
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
       <div class="text-center">
         <h1 class="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-          {{ title }}
+          {{ props.title }}
         </h1>
-        <p class="my-4 text-xl text-white">{{ subtitle }}</p>
+        <p class="my-4 text-xl text-white">{{ props.subtitle }}</p>
       </div>
     </div>
   </section>
