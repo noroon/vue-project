1f8734501e3759f1729745cdd1db09401b45bc1d
noroon <noncha.dev@pm.me>
2026-03-04 15:27:06 +0100

    feat(homepage): add joblistings

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import AppNavbar from '@/components/AppNavbar.vue'
 import AppHero from '@/components/AppHero.vue'
 import HomeCards from '@/components/HomeCards.vue'
+import JobListings from './components/JobListings.vue'
 </script>
 
 <template>
   <AppNavbar />
   <AppHero />
   <HomeCards />
+  <JobListings />
 </template>
