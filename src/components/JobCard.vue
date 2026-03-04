846866f6ee38680c797db9eb6e00518bab27823e
noroon <noncha.dev@pm.me>
2026-03-04 19:31:37 +0100

    feat(jobCard): add line clamp and primeicons

--- src/components/JobCard.vue
+++ src/components/JobCard.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import type { Job } from '@/types/job'
+import { ref } from 'vue'
 
-defineProps<{
+const { job } = defineProps<{
   job: Job
 }>()
+
+const showFullDescription = ref(false)
 </script>
 
 <template>
@@ -14,7 +17,18 @@ defineProps<{
         <h3 class="text-xl font-bold">{{ job.title }}</h3>
       </div>
 
-      <div class="mb-5">{{ job.description }}</div>
+      <div class="mb-5">
+        <div :class="showFullDescription ? '' : 'line-clamp-3'">
+          {{ job.description }}
+        </div>
+        <!-- don't show the button if job.description is less than 3 lines -->
+        <button
+          class="flex justify-self-end text-green-500 hover:text-green-600 text-sm"
+          @click="showFullDescription = !showFullDescription"
+        >
+          {{ showFullDescription ? 'Show less' : 'Show more' }}
+        </button>
+      </div>
 
       <h3 class="text-green-500 mb-2">{{ job.salary }} / Year</h3>
 
@@ -22,9 +36,10 @@ defineProps<{
 
       <div class="flex flex-col lg:flex-row justify-between mb-4">
         <div class="text-orange-700 mb-3">
-          <i class="fa-solid fa-location-dot text-lg"></i>
+          <i class="pi pi-map-marker"></i>
           {{ job.location }}
         </div>
+
         <a
           :href="'/job/' + job.id"
           class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
