26a90a61c6b263177641cd01a5bfc401c954fb23
noroon <noncha.dev@pm.me>
2026-03-05 15:03:39 +0100

    feat(router): add router

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import AppHero from '@/components/AppHero.vue'
+import HomeCards from '@/components/HomeCards.vue'
+import JobListings from '@/components/JobListings.vue'
+</script>
+
+<template>
+  <AppHero />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
+</template>
