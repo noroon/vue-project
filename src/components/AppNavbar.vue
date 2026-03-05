5ea0af9e5008899d2164deb73b07ba8d457d254c
noroon <noncha.dev@pm.me>
2026-03-05 13:41:18 +0100

    style(navbar): make navbar sticky

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -3,7 +3,7 @@ import logo from '@/assets/img/logo.png'
 </script>
 
 <template>
-  <nav class="bg-green-700 border-b border-green-500">
+  <nav class="sticky top-0 bg-green-700 border-b border-green-500">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="flex h-20 items-center justify-between">
         <div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
