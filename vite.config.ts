37ce6090c73271e008069bc4a25aaa5db07d847e
noroon <noncha.dev@pm.me>
2026-03-11 14:50:28 +0100

    feat(JobView, vite.config): add single job view and proxy

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,13 @@ export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   resolve: {
     alias: {
