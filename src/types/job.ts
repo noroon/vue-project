6e1ae8276784d245caf4842bf9edd44856cfb45f
noroon <noncha.dev@pm.me>
2026-03-20 21:14:16 +0100

    feat(): add add job page view

--- src/types/job.ts
+++ src/types/job.ts
@@ -1,7 +1,7 @@
 export type Job = {
   id: string
   title: string
-  type: string
+  type: 'Part-Time' | 'Full-Time' | 'Remote' | 'Internship'
   description: string
   location: string
   salary: string
